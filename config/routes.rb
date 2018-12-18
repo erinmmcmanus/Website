f14a2fc36543daa7e95649809a9cb05fe597f4a5
erinmmcmanus <43481209+erinmmcmanus@users.noreply.github.com>
2018-12-17 18:42:29 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   end
 
   resources :resources_page
+  
+  get '/resources_page/math.html.erb', to: 'resources_page#math'
+  get '/resources_page/science.html.erb', to: 'resources_page#science'
+  get '/resources_page/foreignlanguage.html.erb', to: 'resources_page#foreignlanguage'
+  get '/resources_page/english.html.erb', to: 'resources_page#english'
+  get '/resources_page/history.html.erb', to: 'resources_page#history'
+  get '/resources_page/standardizedtest.html.erb', to: 'resources_page#standardizedtest'
 
   root 'welcome#index'
 end
